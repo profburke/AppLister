ebc2961bf608ce7e52c171dd7cea78a97ed1c97e
Matthew M. Burke <matthew@logicnets.com>
2015-01-08 09:08:47 -0500

    update copyright notice

--- AppListerTests/AppListerTests.swift
+++ AppListerTests/AppListerTests.swift
@@ -3,7 +3,7 @@
 //  AppListerTests
 //
 //  Created by Matthew Burke on 11/12/14.
-//  Copyright (c) 2014 BlueDino Software. Availble under the MIT License. See the file, LICENSE, for details.
+//  Copyright (c) 2014-2015 BlueDino Software. Availble under the MIT License. See the file, LICENSE, for details.
 //
 
 import UIKit
